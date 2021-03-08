66b7051e6e8333990729dcd33202dca4b307ea41
RonRamal <59611500+RonRamal@users.noreply.github.com>
2021-03-08 16:10:34 +0200

    Add project files.

---
+++ TwitterAPI/Controllers/TwitterController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using TwitterAPI.Models;
+
+namespace TwitterAPI.Controllers
+{
+    public class TwitterController : ApiController
+    {
+        //// GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        [HttpGet]
+        public async Task<object> Get([FromUri] string request,[FromUri] int RetweetMin)
+        {
+            TwitterModel tm = new TwitterModel();
+            return await tm.test(request, RetweetMin);
+        }
+
+    
+        // GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TwitterAPI/Models/TwitterModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Parameters;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters.Enum;
+using Tweetinvi.Parameters.V2;
+
+namespace TwitterAPI.Models
+{
+
+    public class TwitterModel
+    {
+        const string ApiKey = "IS2fcuHkE8Cmvoq7eeErjGFyW";
+        const string AccessTokenSecret= "m0HOCvV9jB6PDgcYvFVZMaXIgzc6mJ4E6GAZhSvwjXDxg"; 
+        const string AccessToken= "1349014948106670084-vbVbV6SzbRh4RH8u6putpqOQDGG5Yx";
+        const string ApiKeySecret= "xBZ6eTfKTJULxwiCQz5s7cSNeoEv5wnDDRHqfB42RwQ7rewl82";
+
+        public TwitterModel()
+        {
+
+        }
+
+        //public async Task2<object> test()
+        //{
+        //    var tc = new TwitterClient(ApiKey, ApiKeySecret, AccessToken, AccessTokenSecret);
+        //    var parameters = new SearchTweetsParameters("#Gaming")
+        //    {
+        //        IncludeEntities = true,
+        //        Lang = LanguageFilter.English,
+        //        SearchType = SearchResultType.Mixed,
+        //        TweetMode = TweetMode.Extended
+        //    };
+        //    var tweets = await tc.Search.SearchTweetsAsync(parameters);
+        //    return tweets;
+        //}
+
+        public async Task<object> test(string Query,int RetweetMin)
+        {
+            var tc = new TwitterClient(ApiKey, ApiKeySecret, AccessToken, AccessTokenSecret);
+            // var parameters = new SearchTweetsParameters("#UNBOXING tech min_retweets:2")     
+             var parameters = new SearchTweetsParameters(Query + " min_retweets:"+ RetweetMin)          
+            {
+                IncludeEntities = true,
+                Lang = LanguageFilter.English,
+                SearchType = SearchResultType.Mixed,
+                TweetMode = TweetMode.Extended,                      
+            };
+            var tweets = await tc.Search.SearchTweetsAsync(parameters);
+            return tweets;
+            
+        }
+    }
+}
